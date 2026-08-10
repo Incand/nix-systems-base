fedb0248a4bac93ca386dae8b392b6209aca19a5
Armin Schaare <arminschaare@gmail.com>
2026-08-10 18:03:27 +0200

    fix: add braille dots for git gutter upper and lower delete

--- modules/programs/ide/neovim/general/git.nix
+++ modules/programs/ide/neovim/general/git.nix
@@ -7,7 +7,14 @@
         config = ''
           vim.g.mapleader = " "
           local gs = require('gitsigns')
-          gs.setup({ current_line_blame = false })
+          gs.setup({
+            current_line_blame = false,
+            signs = {
+              delete       = { text = '_' },
+              topdelete    = { text = '^' },
+              changedelete = { text = '_' },
+            },
+          })
           vim.keymap.set('n', '<leader>b', function() gs.blame_line() end, {})
         '';
       }
